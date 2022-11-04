ebb87a3a1d9b435e04c81332abbd7b5de23740bd
AshishChaudhary3639 <ashishchaudhary3639@gmail>
2022-11-04 21:32:26 +0530

    one more project added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,6 +11,7 @@ import {
 import React from "react";
 import { BsGithub } from "react-icons/bs";
 import {  DiJavascript1, DiReact } from "react-icons/di";
+import {TbBrandFirebase} from "react-icons/tb";
 import { SiRedux, SiChakraui } from "react-icons/si";
 import { IoMdArrowDropupCircle } from "react-icons/io";
 
@@ -19,6 +20,89 @@ const Projects = () => {
     <>
       <VStack bg="whitesmoke" w="90%" m="auto" h="auto" pb="40px">
         <Heading>Projects</Heading>
+        <Box
+          w="80%"
+          mt="2rem"
+          boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+          p="10px"
+        >
+          <Flex justifyContent="space-between" mb="20px">
+            <Text as='b' fontSize='2xl'>Milaap</Text>
+            <Flex gap="10px">
+              <Box
+                data-content="REACT"
+                fontSize="30px"
+                borderRadius="7px"
+                boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+                bg="yellow"
+              >
+                <DiJavascript1 color="black" />
+              </Box>
+              <Box
+                data-content="REACT"
+                fontSize="30px"
+                borderRadius="7px"
+                bg="#222222"
+                boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+              >
+                <DiReact color="skyblue" />
+              </Box>
+              <Box
+                data-content="REACT"
+                fontSize="30px"
+                borderRadius="7px"
+                boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+              >
+                <SiRedux color="skyblue" />
+              </Box>
+              <Box
+                data-content="REACT"
+                fontSize="30px"
+                borderRadius="7px"
+                boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+              >
+                <TbBrandFirebase color="yellow" />
+              </Box>
+              <Box
+                data-content="REACT"
+                fontSize="30px"
+                borderRadius="7px"
+                boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+              >
+                <SiChakraui color="skyblue" />
+              </Box>
+            </Flex>
+          </Flex>
+          <Image src={process.env.PUBLIC_URL + "/milaap.PNG"} />
+          <Flex gap="2rem" justifyContent="center">
+            <Link
+              href="https://github.com/AshishChaudhary3639/milaap-clone"
+              isExternal
+            >
+              <Button
+                leftIcon={<BsGithub />}
+                colorScheme="lightgray"
+                variant="outline"
+              >
+                Code
+              </Button>
+            </Link>
+            <Link
+              href="https://6365231ddc409e0299100e43--frabjous-profiterole-b917b5.netlify.app/"
+              isExternal
+            >
+              <Button
+                leftIcon={<IoMdArrowDropupCircle />}
+                colorScheme="lightgray"
+                variant="outline"
+              >
+                Live
+              </Button>
+            </Link>
+          </Flex>
+        </Box>
+      </VStack>
+      <VStack bg="whitesmoke" w="90%" m="auto" h="auto" pb="40px">
         <Box
           w="80%"
           mt="2rem"
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -2,8 +2,9 @@ import { Box, Flex, Heading, VStack } from "@chakra-ui/react";
 import React from "react";
 import { AiFillHtml5 } from "react-icons/ai";
 import { DiCss3, DiJavascript1, DiReact } from "react-icons/di";
-import { SiRedux, SiChakraui } from "react-icons/si";
-import { BsFillBootstrapFill, BsWordpress } from "react-icons/bs";
+import { SiRedux, SiChakraui,SiExpress,SiMongodb } from "react-icons/si";
+import { BsFillBootstrapFill } from "react-icons/bs";
+import {FaNode} from "react-icons/fa"
 import styles from "./Skills.module.css";
 
 const Skills = () => {
@@ -54,7 +55,7 @@ const Skills = () => {
 
         <Box
           data-content="CSS"
-          fontSize="110px"
+          fontSize="100px"
           borderRadius="7px"
           boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
           // _hover={{
@@ -67,7 +68,7 @@ const Skills = () => {
         </Box>
         <Box
           data-content="JS"
-          fontSize="110px"
+          fontSize="100px"
           borderRadius="7px"
           boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
           // _hover={{
@@ -80,7 +81,7 @@ const Skills = () => {
         </Box>
         <Box
           data-content="REACT"
-          fontSize="110px"
+          fontSize="100px"
           borderRadius="7px"
           boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
           // _hover={{
@@ -89,6 +90,18 @@ const Skills = () => {
         >
           <div className={styles.skillSpin}>
             <DiReact color="skyblue" className={styles.skills_grid_item}/>
+          </div>
+        </Box>
+        <Box
+          data-content="REDUX"
+          
+          fontSize="100px"
+          borderRadius="7px"
+          boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+       
+        >
+          <div className={styles.skillSpin}>
+            <SiRedux color="#764abc" className={styles.skills_grid_item}/>
           </div>
         </Box>
       </Flex>
@@ -113,21 +126,10 @@ const Skills = () => {
           lg: "70%",
         }}
       >
-        <Box
-          data-content="REDUX"
-          
-          fontSize="110px"
-          borderRadius="7px"
-          boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
        
-        >
-          <div className={styles.skillSpin}>
-            <SiRedux color="#764abc" className={styles.skills_grid_item}/>
-          </div>
-        </Box>
         <Box
           data-content="CHAKRA"
-          fontSize="110px"
+          fontSize="100px"
           borderRadius="7px"
           boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
           
@@ -138,7 +140,7 @@ const Skills = () => {
         </Box>
         <Box
           data-content="BOOTSTRAP"
-          fontSize="110px"
+          fontSize="100px"
           borderRadius="7px"
           boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
         
@@ -148,13 +150,33 @@ const Skills = () => {
           </div>
         </Box>
         <Box
-          data-content="WORDPRESS"
-          fontSize="110px"
+          data-content="NODEJS"
+          fontSize="100px"
           borderRadius="7px"
           boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
         >
           <div className={styles.skillSpin}>
-            <BsWordpress color="#6fcad0" className={styles.skills_grid_item}/>
+            <FaNode color="#026e00" className={styles.skills_grid_item}/>
+          </div>
+        </Box>
+        <Box
+          data-content="EXPRESS"
+          fontSize="100px"
+          borderRadius="7px"
+          boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+        >
+          <div  className={styles.skillSpin}>
+            <SiExpress  className={styles.skills_grid_item}/>
+          </div>
+        </Box>
+        <Box
+          data-content="MONGODB"
+          fontSize="100px"
+          borderRadius="7px"
+          boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+        >
+          <div className={styles.skillSpin}>
+            <SiMongodb color="#00ed64" className={styles.skills_grid_item}/>
           </div>
         </Box>
       </Flex>
